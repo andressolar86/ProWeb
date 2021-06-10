8999532245d5729b0a8fb251d80549e2fb731737
andressolar86 <andysebassolarte@gmail.com>
2021-06-10 00:04:43 -0500

    https://github.com/andressolar86/Grupo01-TiendaMacotas.git

--- index.php
+++ index.php
@@ -1,2 +1,132 @@
-Hola Mundo Andres
-Hola Mundo
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <a class="navbar-brand">Home</a>
+        <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div id="my-nav" class="collapse navbar-collapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Historia <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Trayectoria <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Contactenos</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container">
+        <br>
+        <br>
+        <br>
+        <div class="alert alert-info">
+            <h1>Hola Chicos</h1>
+        </div>
+        <br>
+        <div class="alert alert-danger">
+            <h1>Hola Chicos</h1>
+        </div>
+        <br>
+        <div class="alert alert-success">
+            <h1>Hola Chicos</h1>
+        </div>
+        <div class="row">
+            <div class="col-3">
+            <div class="card">
+                <img class="card-img-top" src="blob:https://web.whatsapp.com/54b7e782-803c-41b6-ad6c-95145f7b98ef" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">Titulo</h5>
+                    <p class="card-text">content</p>
+                    <button class="btn btn-primary" type="button">COMPRAR</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card">
+                <img class="card-img-top" src="https://static.iris.net.co/soho/upload/images/2020/2/10/61074_1.jpg" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">Titulo 2</h5>
+                    <p class="card-text">content 2</p>
+                </div>
+            </div>
+        </div>
+            <div class="col-3">
+            <div class="card">
+                <img class="card-img-top" src="blob:https://web.whatsapp.com/54b7e782-803c-41b6-ad6c-95145f7b98ef" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">Titulo</h5>
+                    <p class="card-text">content</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card">
+                <img class="card-img-top" src="https://static.iris.net.co/soho/upload/images/2020/2/10/61074_1.jpg" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">Titulo</h5>
+                    <p class="card-text">content</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Title</h5>
+                <p class="card-text">Content</p>
+            </div>
+        <img class="card-img-bottom" src="https://cdn.veloce.it/wp-content/uploads/2019/06/lamborghini-huracanevo531568.jpg" alt="">
+        </div>
+        </div>
+        <div class="col-3">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Title</h5>
+                <p class="card-text">Content</p>
+            </div>
+        <img class="card-img-bottom" src="https://cdn.veloce.it/wp-content/uploads/2019/06/lamborghini-huracanevo531568.jpg" alt="">
+        </div>
+        </div>
+        <div class="col-3">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Title</h5>
+                <p class="card-text">Content</p>
+                <button class="btn btn-primary" type="button">Comprar</button>
+            </div>
+        <img class="card-img-bottom" src="https://cdn.veloce.it/wp-content/uploads/2019/06/lamborghini-huracanevo531568.jpg" alt="">
+        </div>
+        </div>
+        <div class="col-3">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Title</h5>
+                <p class="card-text">Content</p>
+            </div>
+        <img class="card-img-bottom" src="https://cdn.veloce.it/wp-content/uploads/2019/06/lamborghini-huracanevo531568.jpg" alt="">
+        </div>
+        </div>
+        <br>
+        <br>
+        <div class="alert alert-warning">
+            <h1>Hola Chicos</h1>
+        </div>
+    </div>
+</body>
+</html> 
